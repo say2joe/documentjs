2bd192cd95f445a79a85b6ca44d96279dc40096e
David Luecke <daff@neyeon.com>
2012-10-22 13:30:38 -0600

    Porting more DocumentJS to Node

--- test/documentjs.js
+++ test/documentjs.js
@@ -1,24 +1,3 @@
-var expect = require('expect.js');
-// var Script = require('../lib/script');
+var DocumentJS = require('../lib/documentjs.js');
 
-var fs = require('fs');
-var walk    = require('walk');
-var files   = [];
-
-// Walker options
-var walker  = walk.walk('./test', { followLinks: false });
-
-walker.on('file', function(root, stat, next) {
-	var filename = root + '/' + stat.name;
-	// Add this file to the list of files
-	if(/\.js$/.test(filename)) {
-		fs.realpath(filename, function(err, path) {
-			files.push(path);
-			next();
-		});
-	}
-});
-
-walker.on('end', function() {
-	console.log(files);
-});
+DocumentJS('./test/fixtures');
---
+++ test/fixtures/other.js
@@ -0,0 +1,26 @@
+
+/**
+ * @class Stuff
+ * @parent index
+ *
+ * The test
+ *
+ * @param test
+ * @constructor
+ */
+function Thinger(test) {
+
+}
+
+/**
+ * @prototype
+ */
+
+/**
+ *
+ * @param first
+ * @param second
+ */
+Thinger.prototype.test = function(first, second) {
+
+}
